94b24bc263b58c76024acf3c09bfaf600f4e9267
pedroivobrito <pedroivo1020@gmail.com>
2023-09-21 18:44:10 -0300

    First Exercise

---
+++ MinPartitions/MinPartitions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinPartitions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            char[] chars = { 'a', 'b', 'c', };
+
+            for(int i = 0; i < chars.Length; i++)
+            {
+
+            }
+        }
+    }
+}
