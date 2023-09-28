ead5d9b733cc7406778e5027d7159c42db5456d5
pedroivobrito <pedroivo1020@gmail.com>
2023-09-28 14:20:48 -0300

    Adjust Code and Add new Problems

---
+++ MinPartitions/MinPartitions/Functions/Strings/StringProblems.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Functions
+{
+    public class StringProblems
+    {
+        public int MinPartitions(string n)
+        {
+            char[] ch = n.ToCharArray();
+
+            return (int)Char.GetNumericValue(ch.Max());
+        }
+
+        public string DefangIPaddr(string address)
+        {
+            return address.Replace(".", "[.]");
+        }
+
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            int result = 0;
+            foreach (string op in operations)
+            {
+                if (op == "++X" || op == "X++")
+                    result++;
+                if (op == "--X" || op == "X--")
+                    result--;
+            }
+
+            return result;
+        }
+
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            int result = 0;
+
+            foreach (char stone in stones)
+            {
+                if (jewels.Contains(stone))
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
--- MinPartitions/MinPartitions/Program.cs
+++ MinPartitions/MinPartitions/Program.cs
@@ -1,25 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LeetCode.Functions;
+using System;
 
-namespace MinPartitions
+namespace Problems
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-
-            char[] chars = { 'a', 'b', 'c', };
-
-            for(int i = 0; i < chars.Length; i++)
+            do
             {
+                Console.WriteLine("Hello type the problem code!");
+                int code = Int32.Parse(Console.ReadLine());
 
-            }
+                switch (code)
+                {
+                    case 1689:
+                        Console.WriteLine("Partitioning Into Minimum Number Of Deci - Binary Numbers");
+                        Console.WriteLine("Type the initial value:");
+                    
+                        string input1689 = Console.ReadLine();
+                        Console.WriteLine("Result: " + new StringProblems().MinPartitions(input1689));
+
+                        break;
+                    case 1108:
+                        Console.WriteLine("Defanging an IP Address");
+                        Console.WriteLine("Type the initial address:");
+
+                        string address1108 = Console.ReadLine();
+                        Console.WriteLine("Result: " + new StringProblems().DefangIPaddr(address1108));
+
+                        break;
+                    case 2011:
+                        Console.WriteLine("Final Value of Variable After Performing Operations");
+                        Console.WriteLine("Type the initial array serating the values by spaces:");
+
+                        string input2011 = Console.ReadLine();
+                        string[] array2011 = input2011.Split(' ');
+                        Console.WriteLine("Result: " + new StringProblems().FinalValueAfterOperations(array2011));
+
+                        break;
+                    case 771:
+                        Console.WriteLine("Jewels and Stones");
+                        Console.WriteLine("Type the Jewels values:");
+                        string jewels = Console.ReadLine();
+
+                        Console.WriteLine("Type the Stones values:");
+                        string stones = Console.ReadLine();
+
+                        Console.WriteLine("Result: " + new StringProblems().NumJewelsInStones(jewels, stones));
+
+                        break;
+                }
+            }while(true);
         }
     }
 }
